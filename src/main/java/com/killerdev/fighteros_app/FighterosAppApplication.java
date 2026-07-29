1c0e9ad413491bb51094bdb44f0375ad1bb7093f
michelmanquenir <michel.alejandro1994@gmail.com>
2026-07-28 23:21:48 -0400

    first commit

---
+++ src/main/java/com/killerdev/fighteros_app/FighterosAppApplication.java
@@ -0,0 +1,13 @@
+package com.killerdev.fighteros_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FighterosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FighterosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/killerdev/fighteros_app/FighterosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killerdev.fighteros_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FighterosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
